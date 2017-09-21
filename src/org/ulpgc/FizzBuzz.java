4a76eb0d2c3cc22b921b6442d0d50bc5ce08b7dc
aytha <aythamilopezdeniz@hotmail.com>
2017-09-21 09:20:14 +0100

    kata 1

--- src/org/ulpgc/FizzBuzz.java
+++ src/org/ulpgc/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.ulpgc;
 
 public class FizzBuzz {
     public String get(int position) {
-        if(position%15==0)return "org.ulpgc.FizzBuzz";
+        if(position%15==0)return "FizzBuzz";
         else if(position%5==0)return "Buzz";
         return (position%3==0) ? "Fizz" : String.valueOf(position);
     }
